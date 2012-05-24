8e821755d34360d030983d3a5b731439dd5514ce
Nils Wloka <mail@nilswloka.com>
2012-05-24 13:24:14 +0200

    Changed to Select from setSelected

--- src/test/java/com/opitzconsulting/bwertr/spec/RateAPresentationTest.java
+++ src/test/java/com/opitzconsulting/bwertr/spec/RateAPresentationTest.java
@@ -2,8 +2,8 @@ package com.opitzconsulting.bwertr.spec;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class RateAPresentationTest extends AbstractBwertrSpec {
 
@@ -13,9 +13,9 @@ public class RateAPresentationTest extends AbstractBwertrSpec {
         WebDriver webDriver = new HtmlUnitDriver();
         webDriver.get("http://localhost:8080");
         // * Select option and submit
-        WebElement option = webDriver.findElement(By.id(rating));
-        option.setSelected();
-        option.submit();
+        Select select = new Select(webDriver.findElement(By.id("rating")));
+        select.selectByValue(rating);
+        select.getFirstSelectedOption().submit();
         // Return rating shown
         return webDriver.findElement(By.id("givenRating")).getText();
     }
